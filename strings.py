13b23ee9bb51c66dde6249f186208c495ba7b076
Jordan Leigh <151576341+JordannLeigh@users.noreply.github.com>
2024-03-05 02:54:06 +0000

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,16 @@
+"""
+
+start
+
+Declare variable 'hero' containing value "$$$Superman$$$"
+use strip( ) and print hero with output = Superman
+
+stop
+
+"""
+
+
+# Declare variable
+hero = ("$$$Superman$$$")
+# Use strip() to output Superman
+print (hero.strip('$'))
